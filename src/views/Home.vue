7b8f1f10b968b8e93d72e1b9dc260f55adcb9a5a
ww <106657674+weiwang1234@users.noreply.github.com>
2024-12-06 01:40:12 +0800

    页面布局完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,44 @@
     <el-container>
       <!-- Sidebar -->
       <el-aside width="200px" style="background-color: #f4f4f4;">
-        <el-menu :default-active="activeMenu" background-color="#ffffff" text-color="#333" active-text-color="#409EFF">
-          <el-menu-item index="home" @click="navigateTo('/home')">首页</el-menu-item>
-          <el-menu-item index="user-list" @click="navigateTo('/home/product-list')">用户列表</el-menu-item>
-          <el-menu-item index="product-list" @click="navigateTo('/home/product-list')">商品列表</el-menu-item>
+        <el-menu :default-active="activeMenu" background-color="#ffffff" text-color="#333" active-text-color="#409EFF" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <!-- 首页菜单 -->
+          <el-menu-item index="home" @click="navigateTo('/home')">
+            <el-icon><location /></el-icon>
+            <span>首页</span>
+          </el-menu-item>
+
+          <!-- 用户列表菜单 -->
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>用户列表</span>
+            </template>
+            <el-menu-item-group title="Group One">
+              <el-menu-item index="1-1" @click="navigateTo('/home/product-list')">item one</el-menu-item>
+              <el-menu-item index="1-2" @click="navigateTo('/home/1-2')">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3" @click="navigateTo('/home/1-3')">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title>item four</template>
+              <el-menu-item index="1-4-1" @click="navigateTo('/home/1-4-1')">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+
+          <el-menu-item index="2" @click="navigateTo('/home/2')">
+            <el-icon><icon-menu /></el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <el-icon><document /></el-icon>
+            <span>Navigator Three</span>
+          </el-menu-item>
+          <el-menu-item index="4" @click="navigateTo('/home/4')">
+            <el-icon><setting /></el-icon>
+            <span>Navigator Four</span>
+          </el-menu-item>
         </el-menu>
       </el-aside>
 
@@ -26,12 +60,21 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { Document, Menu as IconMenu, Location, Setting } from '@element-plus/icons-vue'
 
 const router = useRouter()
-const activeMenu = ref('home')
+const activeMenu = ref('home') // 默认激活首页菜单
+
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 
 const navigateTo = (path: string) => {
-  router.push(path)
+  router.push(path) // 执行路由跳转
 }
 </script>
 
